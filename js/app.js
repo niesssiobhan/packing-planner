f939b1019970b55b702fd516a3dfff8f4086ffbb
niesssiobhan <niesssiobhan@live.com>
2018-10-30 15:26:50 -0700

    list action progress

--- js/app.js
+++ js/app.js
@@ -1,21 +1,44 @@
 'use strict';
 
-
 //global variables
-var masterItems = ["toiletry bag", "camera", "phone charger", "sunglasses", "wallet/ ID", "water bottle", "medication", "headphones", "sunscreen"];
-var coldActivites = ["sledding", "skiing", "ice fishing", "snowshoeing"];
-var hotAtivities = ["swimming", "snorkeling", "fishing", "golfing", "hiking"];
-var temperateActivities = ["golfing", "hiking", "fishing"];
-var coldMaster = ["heavy jacket", "gloves", "warm boots", "knitted cap", "warm/thick socks", "thermal underwear", "scarf"];
-var hotMaster = ["bathing suit", "beach towel", "sandles/ flip-flops", "hat", "bug spray", "aloe vera/ after sun lotion", "light rain proof jacket"];
-var temperateMaster = ["light rain proof jacket", "hat", "bug spray", "rain boots", "rubber sole shoes/ water-resistant", "umberella"];
-var iceFishing = ["ice auger", "tape measure", "ice pick",];
-var fishingSupplies = ["fishing pole and reel", "tackle box", "first aid kit", "tip up", "life vest", "extra line", "needle nose pliers", "waders"];
-var skiAndSnowboard = ["skiis", "snowboard", "helmet", "boots", "poles", "goggles", "heavy gloves", "heavy jacket", "heavy pants"];
-var snowshoeing = ["snowshoes", "poles", "weather proof boots", "gaiters"];
-var sledding = ["gaiters", "inner tube", "boogie board"];
-var swimming = ["water goggles", "floaties", "nose plug"];
-var hiking = ["tent", "backpack", "hiking boots", "hiking poles", "compass/ map", "food", "cooking pots/ pans", "water filter", "lighter", "utensils", "sleeping bag", "pillow", "first aid kit", "multi-purpose tool", "trash bag"];
-var snorkeling = ["snorkel mask", "flippers", "underwater camera gear", "snorkel", "water shoes", "rash guard", "float vest", "wet suit"];
-var golfing = ["golf shoes", "golf bag", "collared shirt", "golf balls", "tees"];
+var masterItems = ['toiletry bag', 'camera', 'phone charger', 'sunglasses', 'wallet/ ID', 'water bottle', 'medication', 'headphones', 'sunscreen'];
+
+var coldActivities = ['sledding', 'skiing', 'ice fishing', 'snowshoeing'];
+var hotActivities = ['swimming', 'snorkeling', 'fishing', 'golfing', 'hiking'];
+var temperateActivities = ['golfing', 'hiking', 'fishing'];
+
+var coldMaster = ['heavy jacket', 'gloves', 'warm boots', 'knitted cap', 'warm/thick socks', 'thermal underwear', 'scarf'];
+var hotMaster = ['bathing suit', 'beach towel', 'sandles/ flip-flops', 'hat', 'bug spray', 'aloe vera/ after sun lotion', 'light rain proof jacket'];
+var temperateMaster = ['light rain proof jacket', 'hat', 'bug spray', 'rain boots', 'rubber sole shoes/ water-resistant', 'umberella'];
+
+
+var activities = new Map();
+
+activities.set('swimming', ['water goggles', 'floaties', 'nose plug']);
+activities.set('snorkeling', ['snorkel mask', 'flippers', 'underwater camera gear', 'snorkel', 'water shoes', 'rash guard', 'float vest', 'wet suit']);
+activities.set('fishing', ['fishing pole and reel', 'tackle box', 'first aid kit', 'tip up', 'life vest', 'extra line', 'needle nose pliers', 'waders']);
+activities.set('golfing', ['golf shoes', 'golf bag', 'collared shirt', 'golf balls', 'tees']);
+activities.set('hiking', ['tent', 'backpack', 'hiking boots', 'hiking poles', 'compass/ map', 'food', 'cooking pots/ pans', 'water filter', 'lighter', 'utensils', 'sleeping bag', 'pillow', 'first aid kit', 'multi-purpose tool', 'trash bag']);
+activities.set('sledding', ['gaiters', 'inner tube', 'boogie board']);
+activities.set('skiing', ['skiis', 'snowboard', 'helmet', 'boots', 'poles', 'goggles', 'heavy gloves', 'heavy jacket', 'heavy pants']);
+activities.set('ice fishing', ['ice auger', 'tape measure', 'ice pick',]);
+activities.set('showshoeing', ['snowshoes', 'poles', 'weather proof boots', 'gaiters']);
+
 var vacationList = [];
+
+var Trip = function(array) {
+    this.array = array;
+}
+
+var trip = new Trip(masterItems);
+
+Trip.prototype.addTemp = function(temp) {
+    for (var i = 0; i < temp.length; i++) {
+        console.log(temp);
+        this.array.push(temp[i]);
+    }
+}
+
+trip.addTemp(hotMaster);
+trip.addTemp(coldMaster);
+trip.addTemp(temperateMaster);
