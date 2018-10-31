49f3ed1866d2b57cbdf3a0ed26456b7db97158fb
niesssiobhan <niesssiobhan@live.com>
2018-10-31 14:29:31 -0700

    Jared and I got the maunal new item form to work

--- js/packinglist.js
+++ js/packinglist.js
@@ -21,4 +21,23 @@ function renderTrip() {
   }
 }
 
+function handleSubmit(e) {
+e.preventDefault();
+
+var newitem = e.target.newItem.value;
+console.log(newitem);
+listToRender.push(newitem);
+document.getElementById('finalSelection').innerHTML='';
+renderTrip();
+// document.getElementById('add').onclick = function() {
+//   var text = document.getElementById('additions').value;
+//   var li = '<li>' + text + '</li>';
+//   document.getElementById('list').appendChild(li);
+  // listToRender.push(li);
+  // renderTrip();
+  // 
+}
+var additionForm = document.getElementById('additionForm');
+additionForm.addEventListener('submit', handleSubmit);
+
 renderTrip();
